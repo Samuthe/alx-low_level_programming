29b4dc781ef43491bd9a580c3cfc0c70d945828a
Samuthe <ekatisamuel@gmail.com>
2022-04-12 14:41:20 +0300

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-
 /**
- * main - Entry point
- *
- * Return: nothing, return void
- */
+*main - Entry point
+*
+*Return: nothing, return void(success)
+*/
 
 int main(void)
 {
