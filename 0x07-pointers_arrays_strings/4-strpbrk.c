0c06b715c7f21f439311d61ff2a6b48c9ce1938e
Samuthe <ekatisamuel@gmail.com>
2022-04-27 15:11:54 +0300

    4. strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-*_strpbrk - finds first matching char in string
-*@s: input string to search for matching char
-*@accept: characters that could be matched
-*Return: pointer to matching char
+* _strpbrk - finds first matching char in string
+* @s: input string to search for matching char
+* @accept: characters that could be matched
+* Return: pointer to matching char
 */
 
 char *_strpbrk(char *s, char *accept)
