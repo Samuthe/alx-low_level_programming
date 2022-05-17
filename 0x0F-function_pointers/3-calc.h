b6858fd5b11939b4d7c5d5f4a51111ea02a61aa6
Samuthe <ekatisamuel@gmail.com>
2022-05-17 13:59:11 +0300

    calculations

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef OPERATIONS
-#define OPERATIONS
+#ifndef _HEADER_
+#define _HEADER_
 /**
  * struct op - Struct op
  *
@@ -11,6 +11,7 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
