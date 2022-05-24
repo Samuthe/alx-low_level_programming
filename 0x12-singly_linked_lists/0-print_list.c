402255d1873c1beb32b341e027d96e0b5f94eda3
Samuthe <ekatisamuel@gmail.com>
2022-05-24 14:56:06 +0300

    printing lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,18 +10,24 @@ size_t print_list(const list_t *h)
 {
 size_t i;
 
-for(i = 0; h != NULL; i++)
+for (i = 0; h != NULL; i++)
 {
+
 if (h->str != NULL)
 {
+
 printf("[%d] %s\n", h->len, h->str);
 h = h->next;
 }
+
 else
 {
+
 printf("[%d] %s\n", 0, "(nil)");
 h = h->next;
 }
+
 }
+
 return (i);
 }
