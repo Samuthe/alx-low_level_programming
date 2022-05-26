53648550f447e9bf10759b092673945f112e8305
Samuthe <ekatisamuel@gmail.com>
2022-05-26 13:11:20 +0300

    Add node

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,9 +3,9 @@
 size_t
 listint_len(const listint_t *h)
 {
-unsigned int i = 0;
-const listint_t *new_node = h;
-for (; new_node; new_node = new_node->next)
-i++;
-return (i);
+    unsigned int i = 0;
+    const listint_t *new_node = h;
+    for (; new_node; new_node = new_node->next)
+        i++;
+    return (i);
 }
