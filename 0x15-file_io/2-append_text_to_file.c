0ddd9dcea0acba201b7585bac48062912b04b53a
Samuthe <ekatisamuel@gmail.com>
2022-06-02 20:20:17 +0300

    apend

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,53 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
-* main - program that copies the content of a file to another file
-* @argc: num argument
-* @argv: string argument
-* Return: 0
-*/
+ * append_text_to_file - appends text at the end of a file
+ * @filename: path of the file
+ * @text_content: content to append at end of file
+ * Return: 1 on Success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int count, len = 0, fd, i;
 
-int main(int argc, char *argv[])
-{
-int file_from, file_to;
-int num1 = 1024, num2 = 0;
-char buf[1024];
+	if (filename == NULL)
+		return (-1);
 
-if (argc != 3)
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
-}
-file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
-	| S_IRGRP | S_IWGRP | S_IROTH);
-if (file_to == -1)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	close(file_from), exit(99);
-}
-while (num1 == 1024)
-{
-	num1 = read(file_from, buf, 1024);
-	if (num1 == -1)
+	/* Open file and get file descriptor */
+	fd = open(filename, 2 | O_APPEND, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	for (i = 0; text_content[i]; i++)
+		len++;
+
+	/* write into file description */
+	count = write(fd, text_content, len);
+	if (count == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		close(fd);
+		return (-1);
 	}
-	num2 = write(file_to, buf, num1);
-	if (num2 < num1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-}
+	close(fd);
 
-if (close(file_from) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
-
-if (close(file_to) == -1)
-	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-
-return (0);
+	return (1);
 }
