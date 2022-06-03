e80d1c41298bed1afff0fa265021dc935f4ae4c6
Samuthe <ekatisamuel@gmail.com>
2022-06-03 15:28:11 +0300

    appending

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,30 +8,26 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int count, len = 0, fd, i;
+int fd;
+int size = 0;
 
-	if (filename == NULL)
-		return (-1);
+if (filename == NULL)
+return (-1);
 
-	/* Open file and get file descriptor */
-	fd = open(filename, 2 | O_APPEND, 0600);
-	if (fd == -1)
-		return (-1);
+fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (text_content == NULL)
-		return (1);
+if (fd < 0)
+return (-1);
 
-	for (i = 0; text_content[i]; i++)
-		len++;
+if (text_content != NULL)
+{
 
-	/* write into file description */
-	count = write(fd, text_content, len);
-	if (count == -1)
-	{
-		close(fd);
-		return (-1);
-	}
-	close(fd);
+size = strlen(text_content);
+fd = write(fd, text_content, size);
+if (fd < 0)
 
-	return (1);
+return (-1);
+}
+
+return (1);
 }
