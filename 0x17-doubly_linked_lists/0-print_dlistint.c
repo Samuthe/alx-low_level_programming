ac1381f2591bb7f98ded5781cac6661a2bf8f161
Samuthe <ekatisamuel@gmail.com>
2022-07-07 13:51:42 +0300

    doubly linked lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* struct dlistint_s - doubly linked list
+* @n: integer
+* @prev: points to the previous node
+* @next: points to the next node
+*
+* Description: doubly linked list node structure
+*
+*/
+size_t print_dlistint(const dlistint_t *h){
+
+int x;
+for(x = i; h != NULL; x++){
+printf("%i\n", h->n);
+h = h->next;
+}
+return (x);
+}
