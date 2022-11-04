5a93c93a0d7e8a58d2af9423d2d1593ada18d38a
Samuthe <ekatisamuel@gmail.com>
2022-11-04 18:22:07 +0300

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,44 +1,53 @@
 #include "search_algos.h"
 
-/**
-  * print_array - print array with limits in l and r
-  * @array: set of numbers
-  * @l: left limit
-  * @r: right limit
-  * Return: nothing
-  */
-void print_array(int *array, size_t l, size_t r)
-{
-	printf("Searching in array: ");
-	for (l = l; l < r; l++)
-		printf("%d, ", array[l]);
-	printf("%d\n", array[l]);
-}
+void print_array(int *array, size_t i, size_t size);
 
-
-/**
-  * binary_search - search the first ocurrency of a value in the array
-  * @array: set of numbers
-  * @size: size of the array
-  * @value: value to search
-  * Return: return the first index located otherwise -1
-  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t l = 0, r = size - 1, mid = 0;
+    size_t element = 0, biggest = size - 1, half = 0;
 
-	if (array == NULL)
-		return (-1);
-	while (l <= r)
-	{
-		print_array(array, l, r);
-		mid = (l + r) / 2;
-		if (array[mid] < value)
-			l = mid + 1;
-		else if (array[mid] > value)
-			r = mid - 1;
-		else
-			return (mid);
-	}
-	return (-1);
+    if (array == NULL || value == 0)
+        return (-1);
+
+    /*the result from half size of the array*/
+    print_array(array, element, size);
+    /*
+    a[] = 2, 4, 6, 8, 14, 20, 1, 3, 16, 10*/
+    while (element < biggest)
+    {
+        half = (element + biggest) / 2;
+        /*x = 2, 4, 6, 8, 14
+        y = 20, 1, 3, 16, 10*/
+        if (array[half] < value)
+        {
+            element = half + 1;
+            /*
+            l = 2, 4, 6
+            r = 8, 14
+            */
+            print_array(array, element, biggest + 1);
+        }
+        else if (array[half] > value)
+        {
+            biggest = half - 1;
+            print_array(array, element, biggest + 1);
+        }
+        else
+            return (array[half]);
+    }
+    return (-1);
+}
+
+void print_array(int *array, size_t i, size_t size)
+{
+    printf("Searching in array: ");
+    while (i < size)
+    {
+        if (i + 1 == size)
+            printf("%d ", array[i]);
+        else
+            printf("%d, ", array[i]);
+        i++;
+    }
+    printf("\n");
 }
